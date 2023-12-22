d99734d2492d16b707de2de59088911d5de44b23
Leo Barnett <leo.e.barnett@icloud.com>
2023-12-22 09:19:39 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Leo  on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
