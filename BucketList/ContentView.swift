76e268b52246dcd16fe51b1abdebe135ba29ba7d
Leo Barnett <leo.e.barnett@icloud.com>
2024-01-08 14:09:13 +0200

    Version 1.0

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -3,19 +3,55 @@
 //  BucketList
 //
 //  Created by Leo  on 22.12.23.
-//
+//y
 
+import MapKit
 import SwiftUI
 
+
 struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
+    
+    @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onLongPressGesture {
+                                selectedPlace = location
+                            }
+                    }
+                }
+            }
+            .mapStyle(.standard(elevation: .realistic))
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    locations.append(newLocation)
+                }
+            }
+            .sheet(item: $selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ BucketList/EditView.swift
@@ -0,0 +1,52 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by Leo  on 08.01.24.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    @Environment(\.dismiss) var dismiss
+    var location: Location
+    
+    @State private var name: String
+    @State private var description: String
+    var onSave: (Location) -> Void
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Place name", text: $name)
+                    TextField("Description", text: $description)
+                }
+            }
+            .navigationTitle("Place Details")
+            .toolbar {
+                Button("save") {
+                    var newLocation = location
+                    newLocation.id = UUID()
+                    newLocation.name = name
+                    newLocation.description = description
+                    
+                    onSave(newLocation)
+                    dismiss()
+                }
+            }
+        }
+    }
+    
+    init(location: Location, onSave: @escaping (Location) -> Void) {
+        self.location = location
+        self.onSave = onSave
+        
+        _name = State(initialValue: location.name)
+        _description = State(initialValue: location.description)
+    }
+}
+
+#Preview {
+    EditView(location: .example) { _ in }
+}
